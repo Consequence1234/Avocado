68824321b6f5c3c80d7987af56c596a1ad9f0400
Consequence1234 <Brom91@mail.ru>
2020-05-05 03:46:57 +0300

    dorabotka burger

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Добавление класса для Burger, LOCK скролла при Burger//
 $(document).ready(function() {
 	$('.header__burger').click(function(event) {
 		$('.header__burger,.header__menu').toggleClass('active');
@@ -5,6 +6,8 @@ $(document).ready(function() {
 	});
 });
 
+
+// Cтилизация SLIK SLIDER//
 $(document).ready(function(){
 	$(`.slider__body`).slick({
 		arrows: true,
@@ -44,6 +47,7 @@ $(document).ready(function(){
 	});
 });
 
+//Добавляем класс в Слайдер(добавить в Избранное//
 $(document).on('click', '.slider__addtofavorite', function () {
 	let id = $(this).hasClass('simple');
     if (!id) {
@@ -54,11 +58,12 @@ $(document).on('click', '.slider__addtofavorite', function () {
 });
 
 
+//Добавляем и убираем атрибут OPEN для DETAILS при Медиа-запросах//
 var handleMatchMedia = function(mediaQuery) {
 	if (mediaQuery.matches) {
 		$('details').removeAttr('open'); 
 	}  else {
-		$('details').attr('open', 'open');
+		$('details').attr('open', 'open'); //можно убрать т.к. в HTML прописан OPEN для Details.
 	}
 },
 mql = window.matchMedia('(max-width: 767px)');
@@ -66,4 +71,75 @@ handleMatchMedia(mql);
 mql.addListener(handleMatchMedia); 
 
 
+//Аккардеон для DETAILS//
+var details = document.querySelectorAll("details");
+for(i=0;i<details.length;i++) {
+  details[i].addEventListener("toggle", accordion);
+}
+function accordion(event) {
+  if (!event.target.open) return;
+    var details = event.target.parentNode.children;
+    for(i=0;i<details.length;i++) {
+      if (details[i].tagName != "DETAILS" || 
+         !details[i].hasAttribute('open') || 
+         event.target == details[i]) {
+         continue;
+      }
+		details[i].removeAttribute("open");
+    }
+}
 
+//Адаптивная переброска блоков//
+let move_array=[];
+if($('*[data-move]')){
+	$.each($('*[data-move]'), function(index, val) {
+		if($(this).data('move')!='' && $(this).data('move')!=null){
+			$(this).attr('data-move-index',index);
+			move_array[index]={
+				'parent':$(this).parent(),
+				"index":$(this).index()
+			};
+		}
+	});
+}
+function dynamic_adaptive(){
+		let w=$(window).outerWidth();
+	$.each($('*[data-move]'), function(index, val) {
+		if($(this).data('move')!='' && $(this).data('move')!=null){
+				let dat_array=$(this).data('move').split(',');
+				let dat_parent=$('.'+dat_array[0]);
+				let dat_index=dat_array[1];
+				let dat_bp=dat_array[2];
+			if(w<dat_bp){
+				if(!$(this).hasClass('js-move_done_'+dat_bp)){
+					if(dat_index>0){
+						$(this).insertAfter(dat_parent.find('*').eq(dat_index-1));
+					}else{
+						$(this).prependTo(dat_parent);
+					}
+					$(this).addClass('js-move_done_'+dat_bp);
+				}
+			}else{
+				if($(this).hasClass('js-move_done_'+dat_bp)){
+					dynamic_adaptive_back($(this));
+					$(this).removeClass('js-move_done_'+dat_bp);
+				}
+			}
+		}
+	});
+}
+function dynamic_adaptive_back(el){
+	let index_original=el.data('move-index');
+	let move_place=move_array[index_original];
+	let parent_place=move_place['parent'];
+	let index_place=move_place['index'];
+if(index_place>0){
+	el.insertAfter(parent_place.find('*').eq(index_place-1));
+}else{
+	el.prependTo(parent_place);
+}
+}
+$(window).resize(function(event) {
+dynamic_adaptive();
+});
+dynamic_adaptive();
