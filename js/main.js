0a2d3849763567bdd445063c2b53edc811845cd0
Consequence1234 <Brom91@mail.ru>
2020-05-22 04:18:23 +0300

    modal add

--- js/main.js
+++ js/main.js
@@ -90,59 +90,133 @@ function accordion(event) {
 }
 
 //Адаптивная переброска блоков//
-let move_array=[];
-if($('*[data-move]')){
-	$.each($('*[data-move]'), function(index, val) {
-		if($(this).data('move')!='' && $(this).data('move')!=null){
-			$(this).attr('data-move-index',index);
-			move_array[index]={
-				'parent':$(this).parent(),
-				"index":$(this).index()
-			};
-		}
-	});
-}
-function dynamic_adaptive(){
-		let w=$(window).outerWidth();
-	$.each($('*[data-move]'), function(index, val) {
-		if($(this).data('move')!='' && $(this).data('move')!=null){
-				let dat_array=$(this).data('move').split(',');
-				let dat_parent=$('.'+dat_array[0]);
-				let dat_index=dat_array[1];
-				let dat_bp=dat_array[2];
-			if(w<dat_bp){
-				if(!$(this).hasClass('js-move_done_'+dat_bp)){
-					if(dat_index>0){
-						$(this).insertAfter(dat_parent.find('*').eq(dat_index-1));
-					}else{
-						$(this).prependTo(dat_parent);
+(function () {
+	let originalPositions = [];
+	let daElements = document.querySelectorAll('[data-da]');
+	let daElementsArray = [];
+	let daMatchMedia = [];
+	//Заполняем массивы
+	if (daElements.length > 0) {
+		let number = 0;
+		for (let index = 0; index < daElements.length; index++) {
+			const daElement = daElements[index];
+			const daMove = daElement.getAttribute('data-da');
+			if (daMove != '') {
+				const daArray = daMove.split(',');
+				const daPlace = daArray[1] ? daArray[1].trim() : 'last';
+				const daBreakpoint = daArray[2] ? daArray[2].trim() : '767';
+				const daDestination = document.querySelector('.' + daArray[0].trim())
+				if (daArray.length > 0 && daDestination) {
+					daElement.setAttribute('data-da-index', number);
+					//Заполняем массив первоначальных позиций
+					originalPositions[number] = {
+						"parent": daElement.parentNode,
+						"index": indexInParent(daElement)
+					};
+					//Заполняем массив элементов 
+					daElementsArray[number] = {
+						"element": daElement,
+						"destination": document.querySelector('.' + daArray[0].trim()),
+						"place": daPlace,
+						"breakpoint": daBreakpoint
 					}
-					$(this).addClass('js-move_done_'+dat_bp);
-				}
-			}else{
-				if($(this).hasClass('js-move_done_'+dat_bp)){
-					dynamic_adaptive_back($(this));
-					$(this).removeClass('js-move_done_'+dat_bp);
+					number++;
 				}
 			}
 		}
-	});
-}
-function dynamic_adaptive_back(el){
-	let index_original=el.data('move-index');
-	let move_place=move_array[index_original];
-	let parent_place=move_place['parent'];
-	let index_place=move_place['index'];
-if(index_place>0){
-	el.insertAfter(parent_place.find('*').eq(index_place-1));
-}else{
-	el.prependTo(parent_place);
-}
-}
-$(window).resize(function(event) {
-dynamic_adaptive();
-});
-dynamic_adaptive();
+		dynamicAdaptSort(daElementsArray);
+
+		//Создаем события в точке брейкпоинта
+		for (let index = 0; index < daElementsArray.length; index++) {
+			const el = daElementsArray[index];
+			const daBreakpoint = el.breakpoint;
+			const daType = "max"; //Для MobileFirst поменять на min
+
+			daMatchMedia.push(window.matchMedia("(" + daType + "-width: " + daBreakpoint + "px)"));
+			daMatchMedia[index].addListener(dynamicAdapt);
+		}
+	}
+	//Основная функция
+	function dynamicAdapt(e) {
+		for (let index = 0; index < daElementsArray.length; index++) {
+			const el = daElementsArray[index];
+			const daElement = el.element;
+			const daDestination = el.destination;
+			const daPlace = el.place;
+			const daBreakpoint = el.breakpoint;
+			const daClassname = "_dynamic_adapt_" + daBreakpoint;
+
+			if (daMatchMedia[index].matches) {
+				//Перебрасываем элементы
+				if (!daElement.classList.contains(daClassname)) {
+					let actualIndex = indexOfElements(daDestination)[daPlace];
+					if (daPlace === 'first') {
+						actualIndex = indexOfElements(daDestination)[0];
+					} else if (daPlace === 'last') {
+						actualIndex = indexOfElements(daDestination)[indexOfElements(daDestination).length];
+					}
+					daDestination.insertBefore(daElement, daDestination.children[actualIndex]);
+					daElement.classList.add(daClassname);
+				}
+			} else {
+				//Возвращаем на место
+				if (daElement.classList.contains(daClassname)) {
+					dynamicAdaptBack(daElement);
+					daElement.classList.remove(daClassname);
+				}
+			}
+		}
+		customAdapt();
+	}
+
+	//Вызов основной функции
+	dynamicAdapt();
+
+	//Функция возврата на место
+	function dynamicAdaptBack(el) {
+		const daIndex = el.getAttribute('data-da-index');
+		const originalPlace = originalPositions[daIndex];
+		const parentPlace = originalPlace['parent'];
+		const indexPlace = originalPlace['index'];
+		const actualIndex = indexOfElements(parentPlace, true)[indexPlace];
+		parentPlace.insertBefore(el, parentPlace.children[actualIndex]);
+	}
+	//Функция получения индекса внутри родителя
+	function indexInParent(el) {
+		var children = Array.prototype.slice.call(el.parentNode.children);
+		return children.indexOf(el);
+	}
+	//Функция получения массива индексов элементов внутри родителя 
+	function indexOfElements(parent, back) {
+		const children = parent.children;
+		const childrenArray = [];
+		for (let i = 0; i < children.length; i++) {
+			const childrenElement = children[i];
+			if (back) {
+				childrenArray.push(i);
+			} else {
+				//Исключая перенесенный элемент
+				if (childrenElement.getAttribute('data-da') == null) {
+					childrenArray.push(i);
+				}
+			}
+		}
+		return childrenArray;
+	}
+	//Сортировка объекта
+	function dynamicAdaptSort(arr) {
+		arr.sort(function (a, b) {
+			if (a.breakpoint > b.breakpoint) { return -1 } else { return 1 } //Для MobileFirst поменять
+		});
+		arr.sort(function (a, b) {
+			if (a.place > b.place) { return 1 } else { return -1 }
+		});
+	}
+	//Дополнительные сценарии адаптации
+	function customAdapt() {
+		const viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	}
+}());
 
 
 //Адаптив меню под тачскрины//
@@ -183,4 +257,60 @@ function ibg(){
 	}
 	ibg();
 	
+
+//Вызов и закрытие модального окна//
+	$(function() {
+		const modalCall = $("[data-modal]");
+		const modalClose = $("[data-close]");
+
+		modalCall.on("click", function(event) {
+			event.preventDefault();
+
+			let $this = $(this);
+			let modalId = $this.data('modal');
+
+			$(modalId).addClass('show');
+			$("body").addClass('modal_lock');
+
+			setTimeout(function() {
+            $(modalId).find(".login_modal").css({
+            });
+        }, 200);
+
+		});
+		modalClose.on("click", function(event) {
+			event.preventDefault();
+
+			let $this = $(this);
+			let modalParent = $this.parents('.modal');
+
+			modalParent.find(".login_modal").css({
+				
+        });
+
+        setTimeout(function() {
+            modalParent.removeClass('show');
+				$("body").removeClass('modal_lock');
+        }, 600);
+
+		});
+   //Закрытие по области модального окна//
+		$(".modal").on("click", function(event) {
+			let $this = $(this);
+
+		$this.find(".login_modal").css({
+        });
+
+        setTimeout(function() {
+            $this.removeClass('show');
+            $("body").removeClass('modal_lock');
+        }, 600);
+    });
+
+		$(".login_modal, .registration_modal").on("click", function(event) {
+         event.stopPropagation();
+		});
+
+	});
+
 	
